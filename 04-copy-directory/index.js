95571da1aa3245a86cda678c8271ebdacb9aae2d
Ivan Grekov <ivan_grekov@yahoo.com>
2022-11-07 23:08:20 +0300

    feat: add script for task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const path = require("path");
+const { mkdir, readdir, rm, copyFile } = require("fs/promises");
+const initFolder = path.join(__dirname, "files");
+const copiedFolder = path.join(__dirname, "copy-of-files");
 
+async function copyFolder() {
+  await rm(copiedFolder, { recursive: true, force: true });
+  await mkdir(copiedFolder, { recursive: true });
+
+  try {
+    let files = await readdir(initFolder, { withFileTypes: true });
+    for (let file of files) {
+      await copyFile(
+        path.join(initFolder, file.name),
+        path.join(copiedFolder, file.name)
+      );
+    }
+    console.log("Copy folder is created!");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+copyFolder();
