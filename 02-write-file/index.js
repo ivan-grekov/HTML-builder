056db27ff5ef1ae700f6fda4f6240cfb2f690330
Ivan Grekov <ivan_grekov@yahoo.com>
2022-11-08 13:22:33 +0300

    fix: corrected output

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,16 +1,17 @@
 const path = require("path");
 const fs = require("fs");
 const { stdin, stdout } = require("process");
+const ReadLine = require("readline");
 
 const textFile = path.join(__dirname, "text.txt");
-const output = fs.createWriteStream(textFile);
 
 stdout.write("Hi! Enter your text.\n");
+
+const output = fs.createWriteStream(textFile);
 
 stdin.on("data", (data) => {
   data.toString().trim() === "exit" ? process.exit() : output.write(data);
   stdout.write(`${data.toString().trim()} was added!\n`);
-  process.exit();
 });
 
 process.on("exit", () => stdout.write("Have a nice day!\n"));
